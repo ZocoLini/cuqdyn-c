5e3831f5d165d92a874d2de72b1ce142b62d1ec2
Borja Castellano <borjacastellano1@gmail.com>
2025-04-08 11:57:49 +0200

    test and README.md update

--- test.sh
+++ test.sh
@@ -7,21 +7,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-TEST_BINS=(
-  test_matlab
-  test_ode_solver
-)
-
 (
-  cd build/tests || exit 1
-  for test_bin in "${TEST_BINS[@]}"; do
-    echo "Running $test_bin"
-    ./"$test_bin"
-    if [ $? -ne 0 ]; then
-      echo "$test_bin failed"
-      exit 1
-    fi
-  done
-
-  echo "All tests passed"
+  cd build || exit 1
+  ctest
 )
