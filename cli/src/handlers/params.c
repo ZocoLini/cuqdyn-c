01fcc07953d0450ce1cef82760d470147ced69cc
Borja Castellano <borjacastellano1@gmail.com>
2025-04-11 19:10:26 +0200

    fixed bug reading .mat files

--- cli/src/handlers/params.c
+++ cli/src/handlers/params.c
@@ -1,6 +1,7 @@
 #include <matio.h>
 #include <nvector/nvector_serial.h>
 #include <stdio.h>
+#include <string.h>
 #include <sundials/sundials_matrix.h>
 #include <unistd.h>
 
@@ -23,7 +24,7 @@ int handle_params(int argc, char *argv[])
     int opt;
     char *data_file = NULL;
 
-    while ((opt = getopt(argc, argv, "d")) != -1) {
+    while ((opt = getopt(argc, argv, "d:")) != -1) {
         switch (opt) {
             case 'd':
                 data_file = optarg;
@@ -38,10 +39,20 @@ int handle_params(int argc, char *argv[])
     SUNMatrix y = NULL;
 
     if (read_txt_data_file(data_file, &t, &y) != 0
-        || read_mat_data_file(data_file, &t, &y) != 0)
+        && read_mat_data_file(data_file, &t, &y) != 0)
     {
         fprintf(stderr, "Error reading data file: %s\n", data_file);
         return 1;
+    }
+
+    // Bucle para mostrar los datos
+    for (int i = 0; i < N_VGetLength(t); i++)
+    {
+        printf("t[%d] = %g\n", i, NV_Ith_S(t, i));
+        for (int j = 0; j < SUNDenseMatrix_Columns(y); j++)
+        {
+            printf("y[%d][%d] = %g\n", i, j, SM_ELEMENT_D(y, i, j));
+        }
     }
 
     return 0;
@@ -50,36 +61,16 @@ int handle_params(int argc, char *argv[])
 
 int read_txt_data_file(const char *data_file, N_Vector *t, SUNMatrix *y)
 {
-    // TODO: Implement reading from a text file
     FILE *f = fopen(data_file, "r");
-
     if (f == NULL)
     {
         return 1;
     }
 
-    // Reading each not empty line n double values m times
-    int n = 0;
-    int m = 0;
-    char line[1024];
-    while (fgets(line, sizeof(line), f))
-    {
-        if (line[0] == '\n')
-            continue;
-
-        n++;
-        char *ptr = line;
-        while (*ptr != '\0')
-        {
-            if (*ptr == ' ')
-                m++;
-            ptr++;
-        }
-        m++;
-    }
+    // TODO: Not yet implemented
 
     fclose(f);
-    return 0;
+    return 1;
 }
 
 int read_mat_data_file(const char *data_file, N_Vector *t, SUNMatrix *y)
@@ -111,7 +102,6 @@ int read_mat_data_file(const char *data_file, N_Vector *t, SUNMatrix *y)
 
     const size_t rows = matvar->dims[0];
     const size_t cols = matvar->dims[1];
-
     if (rows < 2 || cols < 2)
     {
         fprintf(stderr, "Error: Data must be at least 2x2\n");
@@ -126,18 +116,16 @@ int read_mat_data_file(const char *data_file, N_Vector *t, SUNMatrix *y)
         (sunindextype) rows,
         (sunindextype) cols - 1,
         get_sun_context());
-    sunrealtype *data_y = SUNDenseMatrix_Column(*y, 0);
+    sunrealtype *data_y = SUNDenseMatrix_Data(*y);
 
     double *file_data = matvar->data;
 
     for (int i = 0; i < rows; ++i)
     {
-        data_t[0] = file_data[i * cols];
-
-        // Data cols start at index 1. Index 0 is the time vector
+        data_t[i] = file_data[i];
         for (int j = 1; j < cols; ++j)
         {
-            data_y[i * cols + j - 1] = file_data[i * cols + j];
+            data_y[(j - 1) * rows + i] = file_data[j * rows + i];
         }
     }
 
