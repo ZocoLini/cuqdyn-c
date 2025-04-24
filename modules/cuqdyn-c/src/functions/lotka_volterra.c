e40a06459bcf5edf2bd733edd17effff86810fe3
Borja Castellano <borjacastellano1@gmail.com>
2025-04-24 10:21:58 +0200

    removed example ess solver test

--- modules/cuqdyn-c/src/functions/lotka_volterra.c
+++ modules/cuqdyn-c/src/functions/lotka_volterra.c
@@ -18,6 +18,15 @@ int lotka_volterra_f(realtype t, N_Vector y, N_Vector ydot, void *user_data)
     return (0);
 }
 
+/*
+* function [J,g,R]=prob_mod_lv(x,texp,yexp)
+* [tout,yout] = ode15s(@prob_mod_dynamics_lv,texp,[10,5],odeset('RelTol',1e-6,'AbsTol',1e-6*ones(1,2)),x);
+* R=(yout-yexp);
+* R=reshape(R,numel(R),1);
+* J = sum(sum((yout-yexp).^2));
+* g=0;
+* return
+*/
 void* lotka_volterra_obj_f(double *x, void *data)
 {
     output_function *res = calloc(1, sizeof(output_function));
--- tests/test_ess_solver.c
+++ tests/test_ess_solver.c
@@ -1,6 +1,4 @@
-#define EXAMPLE_CONF_FILE "data/example_ess_config.xml"
 #define LOTKA_VOLTERRA_CONF_FILE "data/lotka_volterra_ess_config.xml"
-#define BBOB_CONF_FILE "data/TEMPLATE_BBOB.xml"
 #define OUPUT_PATH "data/ess_output"
 
 #include <assert.h>
@@ -14,42 +12,7 @@
 #include "ess_solver.h"
 #include "method_module/structure_paralleltestbed.h"
 
-void test_example();
 void lotka_volterra_ess();
-
-void* examplefunction(double *x, void *data) {
-    // Initialize mandatory structs
-    experiment_total *exp1;
-    output_function *res;
-    int dim;
-    exp1 = (experiment_total *) data;
-    res = NULL;
-    res = (output_function *) calloc(1,sizeof(output_function));
-    dim = (*exp1).test.bench.dim;
-    // Objective function code: in this example Schwefel
-    double y, sum;
-    int i;
-    sum = 0.0;
-    for (i=0;i<dim;i++){
-        sum = sum + x[i]*sin(sqrt(abs(x[i])));
-    }
-    y = 418.9829*dim - sum;
-    // Return fx: set the fx in output_function struct
-    // and return like a void pointer
-    res->value = y;
-
-    return res;
-}
-
-/*
-* function [J,g,R]=prob_mod_lv(x,texp,yexp)
-* [tout,yout] = ode15s(@prob_mod_dynamics_lv,texp,[10,5],odeset('RelTol',1e-6,'AbsTol',1e-6*ones(1,2)),x);
-* R=(yout-yexp);
-* R=reshape(R,numel(R),1);
-* J = sum(sum((yout-yexp).^2));
-* g=0;
-return
-*/
 
 int main(int argc, char** argv)
 {
@@ -62,29 +25,10 @@ int main(int argc, char** argv)
     }
 #endif
 
-    test_example();
+    lotka_volterra_ess();
     printf("\tTest 1 passed\n");
 
-    lotka_volterra_ess();
-    printf("\tTest 2 passed\n");
-
     return 0;
-}
-
-void test_example()
-{
-    // I don't know the real values, these are the returned values the test returned for the first time
-    realtype expected[10] = {422, 422, 422, 422, 422, 422, 422, 422, 422, 422};
-
-    N_Vector texp = N_VNew_Serial(0, get_sun_context());
-    DlsMat yexp = SUNDenseMatrix(0, 0, get_sun_context());
-
-    realtype *xbest = execute_ess_solver(EXAMPLE_CONF_FILE, OUPUT_PATH, examplefunction, texp, yexp);
-
-    for (int i = 0; i < 10; ++i)
-    {
-        assert(fabs(xbest[i] - expected[i]) < 3);
-    }
 }
 
 void lotka_volterra_ess()
@@ -108,4 +52,6 @@ void lotka_volterra_ess()
     {
         assert(fabs(xbest[i] - expected[i]) < 0.1);
     }
+
+    destroy_cuqdyn_conf();
 }
