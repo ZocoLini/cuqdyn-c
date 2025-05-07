ebd505ce3f021124b9b17a819c9ee0c760e4a336
Borja Castellano <borjacastellano1@gmail.com>
2025-05-07 08:57:09 +0200

    improved test scripts

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d "build/" ]; then
+    bash build.sh
+fi
+
 (
   cd build || exit 1
   ctest
--- test_cli.sh
+++ test_cli.sh
@@ -2,6 +2,20 @@
 
 CLI_TEST_DIR=build/cli-test
 
+if [ ! -d "build/" ]; then
+    bash build.sh
+fi
+
+if [ ! -d ".venv" ]; then
+    if ! python3 -m venv .venv; then
+        echo "Error creating python virtual environment. Installing python3-venv."
+        sudo apt install python3-venv
+        python3 -m venv .venv
+    fi
+    source .venv/bin/activate
+    pip install --upgrade pip
+fi
+
 rm -r "$CLI_TEST_DIR"
 mkdir "$CLI_TEST_DIR"
 
@@ -12,7 +26,7 @@ cp plot.py "$CLI_TEST_DIR"
 (
   cd "$CLI_TEST_DIR" || exit 1
   ./cli solve -c data/lotka_volterra_cuqdyn_config.xml \
-    -s data/lotka_volterra_ess_config.xml \
+    -s data/lotka_volterra_ess_config_nl2sol.dn2gb.xml \
     -d data/lotka_volterra_paper_data.txt \
     -o data/output \
     -f 0
