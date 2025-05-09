a364972566e4b5849adb379045292e6cce6d8071
Borja Castellano <borjacastellano1@gmail.com>
2025-05-09 11:22:31 +0200

    cli shows the sacess version with the version command

--- modules/cli/src/handlers/version.c
+++ modules/cli/src/handlers/version.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include "cuqdyn_version.h"
+#include "sacess_version.h"
 
 int handle_version(int argc, char *argv[]);
 
@@ -16,7 +17,8 @@ int handle_version(int argc, char *argv[])
 {
     const char *help_message = "- [Version] -\n"
                                "cli %s — Command line interface for the CUQDYN algorithm (by Borja Castellano)\n"
-                               "cuqdyn-c %s — C implementation of the CUQDYN algorithm (by Borja Castellano)\n";
+                               "cuqdyn-c %s — C implementation of the CUQDYN algorithm (by Borja Castellano)\n"
+                               "sacess %s — C implementation of the Scatter Search algorithm\n";
 
-    printf(help_message, CLI_VERSION, CUQDYN_C_VERSION);
+    return printf(help_message, CLI_VERSION, CUQDYN_C_VERSION, SACESS_C_VERSION);
 }
