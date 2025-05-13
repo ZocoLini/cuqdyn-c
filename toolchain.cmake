83a6f4a20940fd1c92044f6aacc81c5bcd64740f
Borja Castellano <borjacastellano1@gmail.com>
2025-05-13 12:57:34 +0200

    removed intel libraries from the mpi build

--- toolchain.cmake
+++ toolchain.cmake
@@ -7,7 +7,7 @@ set(CMAKE_C_FLAGS "-O3 -cpp -DGNU -fPIC -no-pie -DOPENMP -DMPI2 -fopenmp")
 set(CMAKE_Fortran_FLAGS "-O3 -cpp -DGNU  -fallow-argument-mismatch -std=gnu -DOPENMP -DMPI2 -DGNU -fopenmp")
 
 set(MISQP_LIBRARY_DIR "${CMAKE_CURRENT_SOURCE_DIR}/deps/misqp/gnu")
-set(LIBRARIES mpi ifcore ifport imf)
+set(LIBRARIES mpi)
 ######################################################################
 
 ######################################################################
