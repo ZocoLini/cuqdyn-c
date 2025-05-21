9f56d5f99fd9353ba2f8663abb642836c06ae432
Borja Castellano <borjacastellano1@gmail.com>
2025-05-21 12:10:20 +0200

    saving the cuqdyn outputs of the scalability test

--- scripts/scalability-test.sh
+++ scripts/scalability-test.sh
@@ -8,6 +8,7 @@ PROCS_LIST=(2 3 5 6 10 15 30)
 REPEATS=3
 
 # === OUTPUT ===
+mkdir -d "output/scalability"
 OUT_FILE="output/scalability_results.csv"
 echo "Variant,Procs,Iteration,Metric,Value,Unit" > "$OUT_FILE"
 
@@ -25,7 +26,7 @@ for VARIANT in "${VARIANTS[@]}"; do
         -c example-files/lotka_volterra_cuqdyn_config.xml \
         -s example-files/lotka_volterra_ess_"$VARIANT"_config.xml \
         -d example-files/lotka_volterra_paper_data.txt \
-        -o output/
+        -o "output/scalability/$VARIANT/$R"
 
       END_TIME=$(date +%s.%N)
       ELAPSED_TIME=$(echo "$END_TIME - $START_TIME" | bc)
@@ -41,12 +42,12 @@ for VARIANT in "${VARIANTS[@]}"; do
           -c example-files/lotka_volterra_cuqdyn_config.xml \
           -s example-files/lotka_volterra_ess_"$VARIANT"_config.xml \
           -d example-files/lotka_volterra_paper_data.txt \
-          -o output/
+          -o "output/scalability/$VARIANT/$R"
 
         END_TIME=$(date +%s.%N)
         ELAPSED_TIME=$(echo "$END_TIME - $START_TIME" | bc)
 
-        echo "$VARIANT,$PROCS,$R,wall-time,$ELAPSED_TIME,seconds" >> "$OUT_FILE"
+        echo "$VARIANT,$PROCS,$R,wall-time,$ELAPSED_TIME,s" >> "$OUT_FILE"
       done
     done
   fi
