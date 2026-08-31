2035fdc232f1eae16affca83fa822dff2846c953
Borja Castellano <borjacastellano1@gmail.com>
2026-08-31 09:52:17 +0000

    drop mpi2 variant

--- toolchains/mpi_toolchain.cmake
+++ toolchains/mpi_toolchain.cmake
@@ -1,10 +1,12 @@
 set(CMAKE_C_COMPILER mpicc)
 set(CMAKE_CXX_COMPILER mpic++)
 set(CMAKE_Fortran_COMPILER mpifort)
-set(CMAKE_C_FLAGS "-O3 -DGNU -fPIC -no-pie -DMPI -w")
+set(CMAKE_C_FLAGS "-O3 -DGNU -fPIC -no-pie -w")
 set(CMAKE_Fortran_FLAGS
     "-O3 -cpp -DGNU -w -fallow-argument-mismatch -std=gnu -DGNU"
 )
+
+set(CUQDYN_MPI ON)
 
 set(MISQP_LIBRARY_DIR "${CMAKE_CURRENT_SOURCE_DIR}/deps/misqp/gnu")
 set(LIBRARIES mpi)
