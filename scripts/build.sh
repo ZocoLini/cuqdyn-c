c5d11a433db44503fed036b236862c5aaeaf3caa
martarilo <52978045+martarilo@users.noreply.github.com>
2026-09-24 09:42:16 +0200

    Update .gitignore and build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -109,7 +109,7 @@ export CI_JOB_ID=1
 export CI_JOB_STARTED_AT=1
 
 if [ -d "/home/cesga" ]; then
-  module load cesga/2025 gcc/system openmpi/5.0.7 rust/1.88.0
+  module load cesga/2025 gcc/system openmpi/4.1.8 rust/1.88.0
 fi
 
 pids=()
